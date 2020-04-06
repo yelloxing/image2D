282d740b55ed73791cf55967860d0425383fd022
心叶 <yelloxing@gmail.com>
2020-04-06 13:22:17 +0800

    文档v4版本选择iCrush( https://github.com/yelloxing/iCrush )作为框架

--- docs/src/entry.js
+++ docs/src/entry.js
@@ -1,13 +1,10 @@
-import Vue from 'vue';
+import iCrush from 'icrush';
 
 // 兼容文件
 import 'promise-polyfill/src/polyfill';
 
 // 引入启动界面
-import App from './App.vue';
-
-// 引入路由文件
-import router from './router';
+import App from './App.iCrush';
 
 // 引入基础样式
 import '@yelloxing/normalize.css';
@@ -15,14 +12,13 @@ import '@yelloxing/normalize.css';
 // 引入公共样式
 import './style.scss';
 
+document.getElementById('root').innerHTML="";
+
 //根对象
-window.vm = new Vue({
+window.icrush = new iCrush({
 
     //挂载点
     el: document.getElementById('root'),
-
-    // 启用路由
-    router,
 
     // 启动vue
     render: createElement => createElement(App)
