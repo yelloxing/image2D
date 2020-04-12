6c2b11c70d7f2556133ee8a2c24ecdc4287f1581
心叶 <yelloxing@gmail.com>
2020-04-12 13:03:51 +0800

    完成复制功能

--- docs/src/Directives/ui-copy.js
+++ docs/src/Directives/ui-copy.js
@@ -1,11 +1,47 @@
 import iCrush from 'icrush';
 import $$ from 'image2d';
 
+// 错误提示
+let errorback = function () {
+
+};
+
+// 正确提示
+let callback = function () {
+
+};
+
 iCrush.directive('uiCopy', {
 
     inserted: el => {
         $$('<span class="copy-btn" title="复制到剪切板">复制<span>').appendTo(el).bind('click', () => {
-            alert('功能开发中');
+
+            // 初始化准备好结点和数据
+            let random = (Math.random() * 10000).toFixed(0),
+                text = el.getElementsByTagName('pre')[0].innerText,
+                body = document.getElementsByTagName('body')[0],
+                textarea = $$('<textarea id="luna-clipboard-textarea-' + random + '" style="position:absolute">' + text + '</textarea>')[0];
+
+            // 添加到页面
+            body.insertBefore(textarea, body.childNodes[0]);
+
+            // 执行复制
+            document.getElementById("luna-clipboard-textarea-" + random).select();
+            try {
+                let result = window.document.execCommand("copy", false, null);
+
+                if (result) {
+                    callback();
+                } else {
+                    errorback();
+                }
+            } catch (e) {
+                errorback();
+            }
+
+            // 结束后删除
+            body.removeChild(document.getElementById("luna-clipboard-textarea-" + random));
+
         });
     }
 
